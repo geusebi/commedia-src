c41477724c6629f484f5a408290d00d3c9006d33
Giampaolo Eusebi <giampaolo.eusebi@gmail.com>
2019-12-24 23:32:11 +0100

    Add script to automatically generate text and latex versions

---
+++ script/to_latex.py
@@ -0,0 +1,55 @@
+from sys import argv
+
+text = open(argv[1])
+
+root = argv[2]
+start = end = False
+nversi = 0
+lines = filter(None, map(str.strip, text))
+
+for line in lines:
+	if line == ".POEMA":
+		start = True
+		titolo = next(lines)
+
+	if not start:
+		continue
+
+	if line == ".CANTICA":
+		cantica = next(lines)
+		fcantica = open(f"{root}/{cantica.lower()}.tex", "w")
+		fcantica.write(f"\\chapter{{{cantica}}}\n\n")
+
+	if line == ".FINE CANTICA":
+		fcantica.close()
+
+	if line == ".CANTO":
+		canto = next(lines)
+		_, numero = canto.split()
+		fcantica.write(f"\\input{{{cantica.lower()}/{numero}.tex}}\n")
+		fcanto = open(f"{root}/{cantica.lower()}/{numero}.tex", "w")
+		fcanto.write(f"\\section{{{canto}}}\n\n")
+		fcanto.write(f"\\begin{{verse}}\n")
+
+	if line == ".FINE CANTO":
+		fcanto.write(f"\\end{{verse}}\n")
+		fcanto.close()
+
+	if line == ".TERZINA":
+		versi = next(lines), next(lines), next(lines)
+		fcanto.write(f"{versi[0]}\\\\*\n")
+		fcanto.write(f"{versi[1]}\\\\*\n")
+		fcanto.write(f"{versi[2]}\\\\!\n\n")
+		nversi += 3
+
+	if line == ".VERSO":
+		verso = next(lines)
+		fcanto.write(f"{verso}\\\\\n")
+		nversi += 1
+
+	if line == ".FINE":
+		end = True
+		break
+
+assert (nversi == 14233)
+assert (start is True and end is True)
---
+++ script/to_text.py
@@ -0,0 +1,49 @@
+from sys import argv
+
+text = open(argv[1])
+
+start = end = False
+nversi = 0
+lines = filter(None, map(str.strip, text))
+
+for line in lines:
+	if line == ".POEMA":
+		start = True
+		titolo = next(lines)
+		print(f"  {titolo}\n\n")
+
+	if not start:
+		continue
+
+	if line == ".CANTICA":
+		cantica = next(lines)
+		print(f"  {cantica}\n")
+
+	if line == ".FINE CANTICA":
+		print()
+
+	if line == ".CANTO":
+		canto = next(lines)
+		print(f"  {canto}\n")
+
+	if line == ".FINE CANTO":
+		continue
+
+	if line == ".TERZINA":
+		versi = next(lines), next(lines), next(lines)
+		nversi += 3
+		print(f"    {versi[0]}")
+		print(f"    {versi[1]}")
+		print(f"    {versi[2]}\n")
+
+	if line == ".VERSO":
+		verso = next(lines)
+		nversi += 1
+		print(f"    {verso}\n")
+
+	if line == ".FINE":
+		end = True
+		break
+
+assert (nversi == 14233)
+assert (start is True and end is True)
