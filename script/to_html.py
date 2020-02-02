093304ae62b8df4bfb122f2af8f66822f075dc1c
Giampaolo Eusebi <giampaolo.eusebi@gmail.com>
2020-02-02 11:41:25 +0100

    Tabs and spaces

--- script/to_html.py
+++ script/to_html.py
@@ -9,61 +9,61 @@ nversi = 0
 lines = filter(None, map(str.strip, text))
 
 poema = [
-	# {
-	# 	"cantica": "",
-	# 	"canti": [
-	# 		{
-	# 			"canto": "",
-	# 			"versi": []
-	# 		}
-	# 	]
-	# }
+    # {
+    #     "titolo": "",
+    #     "canti": [
+    #         {
+    #             "titolo": "",
+    #             "versi": []
+    #         }
+    #     ]
+    # }
 ]
 
 for line in lines:
-	if line == ".POEMA":
-		start = True
-		titolo = next(lines)
+    if line == ".POEMA":
+        start = True
+        titolo = next(lines)
 
-	if not start:
-		continue
+    if not start:
+        continue
 
-	if line == ".CANTICA":
-		cantica = next(lines)
-		canti = []
-		poema.append({"titolo": cantica, "canti": canti})
+    if line == ".CANTICA":
+        cantica = next(lines)
+        canti = []
+        poema.append({"titolo": cantica, "canti": canti})
 
-	if line == ".FINE CANTICA":
-		pass
+    if line == ".FINE CANTICA":
+        pass
 
-	if line == ".CANTO":
-		canto = next(lines)
-		versi = []
-		canti.append({"titolo": canto, "versi": versi})
+    if line == ".CANTO":
+        canto = next(lines)
+        versi = []
+        canti.append({"titolo": canto, "versi": versi})
 
-	if line == ".FINE CANTO":
-		continue
+    if line == ".FINE CANTO":
+        continue
 
-	if line == ".TERZINA":
-		linee = next(lines), next(lines), next(lines)
-		nversi += 3
-		versi.extend(linee)
+    if line == ".TERZINA":
+        linee = next(lines), next(lines), next(lines)
+        nversi += 3
+        versi.extend(linee)
 
-	if line == ".VERSO":
-		verso = next(lines)
-		nversi += 1
-		versi.append(verso)
+    if line == ".VERSO":
+        verso = next(lines)
+        nversi += 1
+        versi.append(verso)
 
-	if line == ".FINE":
-		end = True
-		break
+    if line == ".FINE":
+        end = True
+        break
 
 assert (nversi == 14233)
 assert (start is True and end is True)
 
 templates_dirs = ["html_templates"]
 if len(argv) > 2:
-	templates_dirs.append(argv[2])
+    templates_dirs.append(argv[2])
 
 loader = FileSystemLoader(templates_dirs)
 env = Environment(loader=loader)
--- script/to_text.py
+++ script/to_text.py
@@ -1,4 +1,4 @@
-from sys import argv
+     sys import argv
 
 text = open(argv[1])
 
@@ -7,43 +7,43 @@ nversi = 0
 lines = filter(None, map(str.strip, text))
 
 for line in lines:
-	if line == ".POEMA":
-		start = True
-		titolo = next(lines)
-		print(f"  {titolo}\n\n")
+    if line == ".POEMA":
+        start = True
+        titolo = next(lines)
+        print(f"  {titolo}\n\n")
 
-	if not start:
-		continue
+    if not start:
+        continue
 
-	if line == ".CANTICA":
-		cantica = next(lines)
-		print(f"  {cantica}\n")
+    if line == ".CANTICA":
+        cantica = next(lines)
+        print(f"  {cantica}\n")
 
-	if line == ".FINE CANTICA":
-		print()
+    if line == ".FINE CANTICA":
+        print()
 
-	if line == ".CANTO":
-		canto = next(lines)
-		print(f"  {canto}\n")
+    if line == ".CANTO":
+        canto = next(lines)
+        print(f"  {canto}\n")
 
-	if line == ".FINE CANTO":
-		continue
+    if line == ".FINE CANTO":
+        continue
 
-	if line == ".TERZINA":
-		versi = next(lines), next(lines), next(lines)
-		nversi += 3
-		print(f"    {versi[0]}")
-		print(f"    {versi[1]}")
-		print(f"    {versi[2]}\n")
+    if line == ".TERZINA":
+        versi = next(lines), next(lines), next(lines)
+        nversi += 3
+        print(f"    {versi[0]}")
+        print(f"    {versi[1]}")
+        print(f"    {versi[2]}\n")
 
-	if line == ".VERSO":
-		verso = next(lines)
-		nversi += 1
-		print(f"    {verso}\n")
+    if line == ".VERSO":
+        verso = next(lines)
+        nversi += 1
+        print(f"    {verso}\n")
 
-	if line == ".FINE":
-		end = True
-		break
+    if line == ".FINE":
+        end = True
+        break
 
 assert (nversi == 14233)
 assert (start is True and end is True)
